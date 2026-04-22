62f227f448dc4937c85e9f85baf68e7becb008bb
i3778323 <i3778323@gmail.com>
2026-04-22 15:27:23 +0000

    Initial commit or update project code

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-import os
+from flask import Flask, render_template
 
-from flask import Flask, send_file
+app = Flask(__name__, template_folder='src')
 
-app = Flask(__name__)
-
-@app.route("/")
+@app.route('/')
 def index():
-    return send_file('src/index.html')
+    return render_template('index.html')
 
-def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
